d85116e91dda53863a7dc34063435dbd5211df25
Neha-KDS <80533750+Neha-KDS@users.noreply.github.com>
2022-10-04 12:50:48 -0400

    including the user pathing code

--- App_user_pathing_analysis.py
+++
@@ -1,8 +0,0 @@
-#Script created by Neha on 2022-10-04
-#Script is designed to analyze Applebees Site traffic analysis on APP only
-#Github repo: https://github.com/matt-walentosky-bt/applebees_user_pathing_analysis
-
-#Import desired packages
-import ga4_user_pathing
-import pandas as pd
-import os
